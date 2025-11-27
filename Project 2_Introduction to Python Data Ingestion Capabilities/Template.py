959eefdc7382322e41bcc2e9d26f32740c594cc2
mcleanse13 <120418919+mcleanse13@users.noreply.github.com>
2025-11-27 17:22:14 -0500

    Add files via upload

---
+++ Project 2_Introduction to Python Data Ingestion Capabilities/Template.py
@@ -0,0 +1,38 @@
+"""
+<Project Title>
+
+
+Copyright (c) 2021 -- This is the 2021 Spring B version of the Template
+Licensed
+Written by <> <---- PLEASE, WRITE YOUR NAME HERE
+
+# you can also rely on the docstring documentation from pandas on how to format dosctrings:
+# https://pandas.pydata.org/pandas-docs/stable/development/contributing_docstring.html
+
+"""
+
+def function1(var1,var2,var3):
+    """
+    :param var1: please describe
+    :param var2: please describe
+    :param var3: please describe
+    :return:
+    """
+
+    return
+
+def function2(var1,var2,var3):
+    """
+    :param var1:
+    :param var2:
+    :param var3:
+    :return:
+    """
+
+    return
+
+if __name__ == "__main__":
+    # Main functions to Run
+	function1(var1=123, var2=456, var3=678)
+
+	
